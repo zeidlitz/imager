ad840f50d0e6af2230da1770ad74ff9eb8606529
zeidlitz <zeidlitzerik@gmail.com>
2024-04-05 21:44:45 +0200

    Initial commit

---
+++ cmd/kvdb/handlers.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/zeidlitz/kvdb/internal/response"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	data := app.newTemplateData(r)
+
+	err := response.Page(w, http.StatusOK, data, "pages/home.tmpl")
+	if err != nil {
+		app.serverError(w, r, err)
+	}
+}
---
+++ cmd/kvdb/middleware.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+
+	"github.com/zeidlitz/kvdb/internal/response"
+
+	"github.com/tomasen/realip"
+)
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			err := recover()
+			if err != nil {
+				app.serverError(w, r, fmt.Errorf("%s", err))
+			}
+		}()
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) securityHeaders(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		w.Header().Set("X-Frame-Options", "deny")
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) logAccess(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mw := response.NewMetricsResponseWriter(w)
+		next.ServeHTTP(mw, r)
+
+		var (
+			ip     = realip.FromRequest(r)
+			method = r.Method
+			url    = r.URL.String()
+			proto  = r.Proto
+		)
+
+		userAttrs := slog.Group("user", "ip", ip)
+		requestAttrs := slog.Group("request", "method", method, "url", url, "proto", proto)
+		responseAttrs := slog.Group("repsonse", "status", mw.StatusCode, "size", mw.BytesCount)
+
+		app.logger.Info("access", userAttrs, requestAttrs, responseAttrs)
+	})
+}
