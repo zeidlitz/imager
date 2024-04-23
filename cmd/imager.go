54d16bf6167bc9a1c8f3e67cc682d63f0e520692
zeidlitz <zeidlitzerik@gmail.com>
2024-04-23 22:39:44 +0200

    Draft structure with call and response

--- cmd/imager.go
+++ cmd/imager.go
@@ -1,32 +1,41 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
+	"encoding/json"
+	"fmt"
+	"net/http"
 )
 
-var(
-  count int
-)
-
-func getJsonResponseBytes(status int, detail string) ([]byte, error) {
-  response := map[string]string{"status": status,  "detail" : detail}
+func getJsonResponseBytes(detail string, code int) ([]byte, error) {
+  statusCode := fmt.Sprintf("%d", code)
+  response := map[string]string{"status": statusCode,  "detail" : detail}
 	jsonResponse, _ := json.Marshal(response)
   return jsonResponse, nil
 }
 
 func indexPage(w http.ResponseWriter, req *http.Request) {
   if(req.Method != http.MethodGet) {
-    statusCode := http.StatusMethodNotAllowed
+
     detail := "Invalid method"
-    msg, _ := getJsonResponseBytes(statusCode, "Invalid method")
+    statusCode := http.StatusMethodNotAllowed
+
     http.Error(w, detail, http.StatusMethodNotAllowed) 
+    msg, _ := getJsonResponseBytes(detail, statusCode)
     w.Write(msg)
+
     return
   }
-  count++
+
+  detail := "OK"
+  statusCode := http.StatusOK
+  msg, _ := getJsonResponseBytes(detail, statusCode)
+
+  w.Write(msg)
 }
 
 func main() {
+  address := "localhost:8080"
+  fmt.Println("Listening on ", address)
   http.HandleFunc("/", indexPage)
+  http.ListenAndServe(address, nil)
 }
